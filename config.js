c1cfd55e8721ebd7abf2523e761f3aa67a326a56
eeshtyagi <eesh.tyagi@gmail.com>
2019-01-02 15:43:20 +0530

    Tweet bot first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'bvXGvNohk2gVPkZUEEGbUSJYE',  
+    consumer_secret: 'W2cj4VvDrXsupGeqCF3sEm58eIiSSTwd9CqL6EivW2MdgW2wef',
+    access_token: '1014749154-nBatQnKNjW5CUUWOey6Sgf3N0KKkObWsm2W9re4',  
+    access_token_secret: 'ZcQbCeUyFb13C8fSu7KMhC8M7kOzHzpJxh0RAy073UH32'
+  }
